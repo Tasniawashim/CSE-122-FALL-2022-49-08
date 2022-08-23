dbee9d75a0f081c808ee93c46b52777f6d7e57fc
Tasnia Washim Bidhu <109478309+Tasniawashim@users.noreply.github.com>
2022-08-23 15:31:01 +0600

    Add files via upload

---
+++ lab 4/2_321_49_8.cpp
@@ -0,0 +1,32 @@
+//Codeforce
+//Problem No : 236A
+//Problem Name : Boy or Girl
+
+#include <iostream>
+#include <string.h>
+using namespace std;
+int main()
+{
+    int i, j, d=0, same=0;
+    char s[100];
+    cin >> s;
+    for(i=0; s[i]!='\0'; i++){
+        for(j=i-1; j>=0; j--){
+            if(s[i]==s[j]){
+                d++;
+                break;
+            }
+        }
+        if(d==0){
+            same++;
+        }
+        d=0;
+    }
+    if(same%2==0){
+        cout<<"CHAT WITH HER!"<< endl;
+    } else {
+        cout<< "IGNORE HIM!" << endl;
+    }
+
+    return 0;
+}
